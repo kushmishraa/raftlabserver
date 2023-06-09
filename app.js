3f64c0504540017e6ad9186c75af514e748189cc
kushmishraa <mishrash64@gmial.com>
2023-06-10 05:28:54 +0530

    cors new

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('./db/conn')
 dotenv.config({path : "./config.env"})
 let gfs;
 app.use(cors({
-    origin : "http://localhost:3000/",
+    origin : "http://localhost:3000",
     credentials: true
 }))
 app.use((req, res, next) => {
