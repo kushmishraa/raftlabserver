9e3d58c2803716d513c4980dc4e6e743a1e0bd00
kushmishraa <mishrash64@gmial.com>
2023-06-10 05:16:29 +0530

    cors *

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors({
     credentials: true
 }))
 app.use((req, res, next) => {
-    res.header({"Access-Control-Allow-Origin": "*,http://localhost:3000/"});
+    res.header({"Access-Control-Allow-Origin": "*"});
     next();
   }) 
 app.use(express.json());
