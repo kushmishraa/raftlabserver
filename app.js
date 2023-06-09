02f815b83d2607859211d836e70813429a1c1fcd
kushmishraa <mishrash64@gmial.com>
2023-06-10 05:00:30 +0530

    cors setup

--- app.js
+++ app.js
@@ -6,22 +6,15 @@ const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser')
 
-
 require('./db/conn')
-
-
-
+dotenv.config({path : "./config.env"})
 let gfs;
 app.use(cors({
-    origin : 'http://localhost:3000/',
+    origin : process.env.BASEURL,
     credentials: true
 }))
 app.use(express.json());
 app.use(cookieParser())
-
-
-dotenv.config({path : "./config.env"})
-dotenv.config({path:'./config.env'})
 
 app.use(require('./routes/routes'))
 
