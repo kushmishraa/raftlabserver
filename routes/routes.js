4ca5bdbbab20be47c1e2d261315309770655ea4f
kushmishraa <mishrash64@gmial.com>
2023-06-11 21:56:17 +0530

    find all

--- routes/routes.js
+++ routes/routes.js
@@ -111,7 +111,8 @@ router.post(`/upload`, upload.single('image') , async (req , res)=>{
         if(req.body.isProfilePic == "true"){
             console.log("profile pic uploading")
             try{
-                const selectedUser = await User.findOne({email : req.body.email});
+                const selectedUser = await User.find({});
+                return;
                 console.log("selected user => " , selectedUser);
                 console.log("image url => " , result.url);
                 selectedUser.profilePicture = result.url;
